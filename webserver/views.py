58a6200a40a3ea484bdffdb9f9fcb8e70547a96f
Oleh Voitovych <144103468+OlehVoitovych@users.noreply.github.com>
2024-04-22 18:46:47 +0300

    /

--- webserver/views.py
+++ webserver/views.py
@@ -27,25 +27,31 @@ def home():
 
 @views.route('/admin_tools', methods=['GET', 'POST'])
 def user_list():
-    users = User.query.all()
-    table = AdminUserEdit(users)
-    table.border = True
-    return render_template("userList.html", table=table)
+    if current_user.role == "Administrator":
+        users = User.query.all()
+        table = AdminUserEdit(users)
+        table.border = True
+        return render_template("userList.html", table=table)
+    else:
+        return redirect(url_for("views.home"))
 
 
 @views.route('/edit_user/<int:id>', methods=['GET', 'POST'])
 def edit_user(id):
-    user = User.query.filter_by(id=id).first()
-    if user:
-        form = UserForm(formdata=request.form, obj=user)
-        if request.method == 'POST' and form.validate():
-            # save edits
-            save_changes_user(user, form)
-            flash('User updated successfully!')
-            return redirect('/admin_tools')
-        return render_template('editForm.html', form=form)
+    if current_user.role == "Administrator":
+        user = User.query.filter_by(id=id).first()
+        if user:
+            form = UserForm(formdata=request.form, obj=user)
+            if request.method == 'POST' and form.validate():
+                # save edits
+                save_changes_user(user, form)
+                flash('User updated successfully!')
+                return redirect('/admin_tools')
+            return render_template('editForm.html', form=form)
+        else:
+            return 'Error loading #{id}'.format(id=id)
     else:
-        return 'Error loading #{id}'.format(id=id)
+        return redirect(url_for("views.home"))
 
 
 def save_changes_user(user, form, new=False):
@@ -62,35 +68,44 @@ def save_changes_user(user, form, new=False):
 
 @views.route('/product_list', methods=['GET', 'POST'])
 def product_list():
-    products = db.session.query(Product).all()
-    table = AdminProductEdit(products)
-    table.border = True
-    return render_template("productList.html", table=table)
+    if current_user.role == "Administrator":
+        products = db.session.query(Product).all()
+        table = AdminProductEdit(products)
+        table.border = True
+        return render_template("productList.html", table=table)
+    else:
+        return redirect(url_for("views.home"))
 
 
 @views.route('/edit_product/<int:id>', methods=['GET', 'POST'])
 def edit_product(id):
-    product = Product.query.filter_by(id=id).first()
-    if product:
-        form = ProductForm(formdata=request.form, obj=product)
-        if request.method == 'POST' and form.validate():
-            save_changes_product(product, form)
-            flash('User updated successfully!')
-            return redirect('/product_list')
-        return render_template('productEditForm.html', form=form)
+    if current_user.role == "Administrator":
+        product = Product.query.filter_by(id=id).first()
+        if product:
+            form = ProductForm(formdata=request.form, obj=product)
+            if request.method == 'POST' and form.validate():
+                save_changes_product(product, form)
+                flash('User updated successfully!')
+                return redirect('/product_list')
+            return render_template('productEditForm.html', form=form)
+        else:
+            return 'Error loading #{id}'.format(id=id)
     else:
-        return 'Error loading #{id}'.format(id=id)
+        return redirect(url_for("views.home"))
 
 
 @views.route('/add_product', methods=['GET', 'POST'])
 def add_product():
-    form = ProductForm(request.form)
-    if request.method == 'POST':
-        db.session.add(
-            Product(form.product_name.data, form.barcode.data, form.product_type.data, form.product_number.data))
-        db.session.commit()
-        return redirect('/product_list')
-    return render_template("productAddForm.html", form=form)
+    if current_user.role == "Administrator":
+        form = ProductForm(request.form)
+        if request.method == 'POST':
+            db.session.add(
+                Product(form.product_name.data, form.barcode.data, form.product_type.data, form.product_number.data))
+            db.session.commit()
+            return redirect('/product_list')
+        return render_template("productAddForm.html", form=form)
+    else:
+        return redirect(url_for("views.home"))
 
 
 def save_changes_product(prod, form, new=False):
